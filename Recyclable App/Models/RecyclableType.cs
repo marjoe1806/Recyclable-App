50ef8cb3b1af01a220ea198f74d0787880c8f3fc
Marjoe aguilar <62055550+marjoe1806@users.noreply.github.com>
2024-06-02 01:28:29 +0000

    Adding all File

---
+++ Recyclable App/Models/RecyclableType.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+using System.Text.Json.Serialization;
+
+namespace Recyclable_App.Services1
+{
+    public class RecyclableTypes
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public decimal Rate { get; set; }
+        public decimal MinKg { get; set; }
+        public decimal MaxKg { get; set; }
+
+    }
+
+ /*   CREATE TABLE RecyclableTypes(
+    Id INT PRIMARY KEY,
+    Type NVARCHAR(255) NOT NULL,
+    Rate DECIMAL(18, 2) NOT NULL,
+    MinKg DECIMAL(18, 2) NOT NULL,
+    MaxKg DECIMAL(18, 2) NOT NULL
+);*/
+
+}
